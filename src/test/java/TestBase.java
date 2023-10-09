82f6d434d313aeb1a45ba4e3ea182659661a0fde
olexandrso <alex.worksot@gmail.com>
2023-10-09 14:52:10 +0300

    First

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -9,10 +9,10 @@ import static com.codeborne.selenide.Selenide.*;
 public class TestBase {
     @BeforeAll  // Annotation to indicate that this method should run before all tests
     static void beforeAll() {
-        browser = "edge";  // Set the browser to Microsoft Edge
+        browser = "chrome";  // Set the browser to Microsoft Edge
         browserSize = "1920x1080";  // Set the browser window size
         timeout = 7000;  // Set the timeout for waiting for elements
-        headless = false;  // Run tests in headless mode
+        headless = true;  // Run tests in headless mode
         baseUrl = "https://www.linkedin.com";  // Set the base URL for tests
         open("");  // Open the base URL
 
